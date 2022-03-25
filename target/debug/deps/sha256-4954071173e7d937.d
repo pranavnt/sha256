08927982bbd43da0dd817472b81761bb3c8c3ed7
Pranav Teegavarapu <pranavnt@outlook.com>
2022-03-25 10:54:19 -0700

    remove target

--- target/debug/deps/sha256-4954071173e7d937.d
+++
@@ -1,5 +0,0 @@
-/Users/pranav/src/pranavnt/sha256/target/debug/deps/sha256-4954071173e7d937.rmeta: src/main.rs
-
-/Users/pranav/src/pranavnt/sha256/target/debug/deps/sha256-4954071173e7d937.d: src/main.rs
-
-src/main.rs:
